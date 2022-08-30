367f0b7033d3a4fa7be0a926baa8c8ca989f3926
Happy <biswasnedhi09@gmail.com>
2022-08-30 11:27:55 -0500

    exercise completed

--- Car/Car.cs
+++ Car/Car.cs
@@ -1,4 +1,7 @@
-﻿namespace CarNS
+﻿using System;
+using static System.Console;
+
+namespace CarNS
 {
     public class Car
     {
@@ -38,6 +41,14 @@
             double gallonsUsed = milesAbleToTravel / MilesPerGallon;
             GasTankLevel -= gallonsUsed;
             Odometer += milesAbleToTravel;
+        }
+        public void AddGas(double gas)
+        {
+            GasTankLevel += gas;
+            if (GasTankLevel > GasTankSize)
+            {
+                throw new ArgumentOutOfRangeException("Can't exceed tank size");
+            }
         }
 
     }
